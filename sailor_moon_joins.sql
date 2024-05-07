5fd6de0c0cfda5567e93d663347fcea460e22e38
Błażej Klepacki <01187238@pw.edu.pl>
2024-05-08 00:15:51 +0200

    fix joins on sailor moon joins task

--- sailor_moon_joins.sql
+++ sailor_moon_joins.sql
@@ -1,9 +1,9 @@
 SELECT slrsc.senshi_name AS sailor_senshi, slrsc.real_name_jpn AS real_name,
        slrsc.cat_name AS cat, s.school AS school
 FROM
-  (SELECT slrs.id, slrs.senshi_name, slrs.real_name_jpn, c.name AS cat_name
-   FROM sailorsenshi slrs
-   LEFT JOIN cats c
-   ON slrs.id = c.id) AS slrsc
-   LEFT JOIN schools AS s
-   ON slrsc.id = s.id;
+  (SELECT slrs.id, slrs.school_id, slrs.senshi_name, slrs.real_name_jpn, c.name AS cat_name
+  FROM sailorsenshi slrs
+  LEFT JOIN cats c
+  ON slrs.cat_id = c.id) as slrsc
+  LEFT JOIN schools s
+  ON slrsc.school_id = s.id
